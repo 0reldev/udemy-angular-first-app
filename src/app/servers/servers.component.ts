1a34aa11a071382b924e341913a34b4681f1fc7d
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-08-08 16:06:03 +0200

    Lesson about selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',  // [app-server] selects the component app-servers as an attribute
+  // selector: '.app-servers',   // .app-servers selects the component as a class, like in CSS
   selector: 'app-servers',
   template: `
     <app-server></app-server>
