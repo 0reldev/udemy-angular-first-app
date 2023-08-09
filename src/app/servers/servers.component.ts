a83bd9d93460bca5426678b6db380eae8874ad22
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-08-09 16:18:40 +0200

    Example of property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',  // [app-server] selects the component app-servers as an attribute
   // selector: '.app-servers',   // .app-servers selects the component as a class, like in CSS
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  
+  allowNewServer = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+  }
 }
