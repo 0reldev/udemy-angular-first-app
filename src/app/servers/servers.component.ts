306325927a5d29b3803913def4f464f1319ad0dd
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-08-09 17:01:50 +0200

    Another example of event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName ='';
 
   constructor() {
     setTimeout(() => {
@@ -26,5 +27,9 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
