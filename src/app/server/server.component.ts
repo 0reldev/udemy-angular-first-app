327c948c358ef30ff77bfb1003db20e6f18eaf3e
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-08-11 15:46:33 +0200

    Example of use of ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,6 +7,11 @@ import { Component } from "@angular/core";
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = "offline";
+    styles: [`
+    .online {
+        color: white
+    }
+    `]
 
     constructor() {
         this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
